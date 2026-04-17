d07dd9d08c18480a7f6bc80357f03fc26a899645
Aran <164043506+w1887705@users.noreply.github.com>
2026-04-17 09:52:49 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
 pip install -r requirements.txt
+python manage.py makemigrations
 python manage.py migrate
 python manage.py seed_demo
 python manage.py collectstatic --noinput
 echo "from django.contrib.auth import get_user_model; U=get_user_model(); U.objects.filter(username='admin').exists() or U.objects.create_superuser('admin','','admin123')" | python manage.py shell
-python manage.py makemigrations
+

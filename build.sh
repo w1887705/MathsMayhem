d98d4fb39fb58d9cd8a40cced28dd629657fecc4
Aran <164043506+w1887705@users.noreply.github.com>
2026-04-17 09:25:06 +0100

    Add superuser creation command to build script
    Add command to create superuser if not exists

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ pip install -r requirements.txt
 python manage.py migrate
 python manage.py seed_demo
 python manage.py collectstatic --noinput
+echo "from django.contrib.auth import get_user_model; U=get_user_model(); U.objects.filter(username='admin').exists() or U.objects.create_superuser('admin','','admin123')" | python manage.py shell
